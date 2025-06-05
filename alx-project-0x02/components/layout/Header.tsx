7550377e81902be7e626d89e7444dbde712a4b8a
hasnaaye <hasnaeyerrou@gmail.com>
2025-06-05 16:32:37 +0100

    Update Header.tsx

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Navbar from './components/Navbar';
-
 const Header: React.FC = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
+    <div>
+      <nav className="bg-gray-800 p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <ul className="flex space-x-4">
+            <li>
+              <a href="/home" className="text-white hover:text-gray-300">Home</a>
+            </li>
+            <li>
+              <a href="/about" className="text-white hover:text-gray-300">About</a>
+            </li>
+          </ul>
+      </nav>
+    </div>
   );
 };
 
 export default Header;
+
