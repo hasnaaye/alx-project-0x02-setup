a7611d7830d3487f425c2b08614f1d211644c52e
hasnaaye <hasnaeyerrou@gmail.com>
2025-06-06 22:53:51 +0100

    Update users.tsx

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,29 +1,33 @@
-export interface UserProps {
-    id:       number;
-    name:     string;
-    username: string;
-    email:    string;
-    address:  Address;
-    phone:    string;
-    website:  string;
-    company:  Company;
+import PostCard from "@/components/common/UserCard";
+import Header from "@/components/layout/Header";
+import { PostProps } from "@/interfaces";
+
+const Users: React.FC<UsersProps[]> = ({ users }) => {
+  console.log(users)
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+        <h1 className=" text-2xl font-semibold">User Content</h1>
+        <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
+        </div>
+        <div className="grid grid-cols-3 gap-2 ">
+          {
+            posts.map(({ name, email, address}: UserProps, key: number) => (
+              <UserCard name={name} email={email} address={address}  />
+            ))
+          }
+        </div>
+      </main>
+    </div>
+  )
 }
 
-export interface Address {
-    street:  string;
-    suite:   string;
-    city:    string;
-    zipcode: string;
-    geo:     Geo;
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+  return { props: { users }};
 }
 
-export interface Geo {
-    lat: string;
-    lng: string;
-}
-
-export interface Company {
-    name:        string;
-    catchPhrase: string;
-    bs:          string;
-}
+export default Users;
