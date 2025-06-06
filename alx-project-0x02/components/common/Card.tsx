32a8fc9590bdc19cfe1fe40369201b7ba2426389
hasnaaye <hasnaeyerrou@gmail.com>
2025-06-06 16:31:46 +0100

    Create Card.tsx

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,9 @@
+
+const Card: React.FC<CardProps> = ({title, content}) => {
+  return (
+    <div className="bg-white shadow-md rounded-2xl p-4 max-w-md">
+      <h2 className="text-xl font-semibold mb-2">{title}</h2>
+      <p className="text-gray-700">{content}</p>
+    </div>
+  );
+};
