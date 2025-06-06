c3937a04ebab729c003b0330d719ccca9ceab749
hasnaaye <hasnaeyerrou@gmail.com>
2025-06-06 22:08:18 +0100

    Update about.tsx

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,2 +1,13 @@
 import Button from '@/components/common/Button';
 import Header from "@/components/layout/Header";
+
+const About:React.FC = () => {
+  return (
+    <>
+      <div>
+        <h1>About page</h1>
+      </div>
+      <Header />     
+        )
+}
+export default About
