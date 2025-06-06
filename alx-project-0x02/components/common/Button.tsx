d97b7e3670afdb078039058a543c26a7a7bc79b8
hasnaaye <hasnaeyerrou@gmail.com>
2025-06-06 18:17:02 +0100

    Create Button.tsx

---
+++ alx-project-0x02/components/common/Button.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const sizeClasses = {
+    small: "px-2 py-1 text-sm",
+    medium: "px-4 py-2 text-base",
+    large: "px-6 py-3 text-lg",
+  };
+const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
+  return (
+    <button 
+      className="px-4 py-2 bg-blue-500 text-white rounded" 
+      onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
